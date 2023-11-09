4c5d39c4e513cd386184420fe4d60d6a6e4ac490
Praveen Tiwari <pr@gbail.com>
2023-11-09 16:18:38 +0530

    test PR

--- ACSApiGateway/src/Adapter/ACSService/ACSUpdateData.js
+++ ACSApiGateway/src/Adapter/ACSService/ACSUpdateData.js
@@ -7,6 +7,7 @@ const { erhandler } = require("@reliablesofttech/bmsmessagehandler");
 export default class ACSUpdateData {
   static async update_ACS(args, CONTEXT) {
     let { URL, sSerialNumber } = args;
+	let sURL ="";
 
     if (URL && sSerialNumber) {
       URL = ACSUpdateURL(args);
